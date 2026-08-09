1d78c5d60b5d06b95bd86251627a5c3c225cbd9a
Najmul Hasan <32976012+najmul-hasan-nirob@users.noreply.github.com>
2026-08-09 10:44:03 +0600

    Remove incomplete Lesson 1 split file before safe migration

--- js/lessons/lesson1.js
+++
@@ -1,8 +0,0 @@
-// lesson1 data
-const lesson1 = {
-
-        vocabulary: [
-            {jp:"わたし", romaji:"watashi", en:"I", bn:"আমি"},
-            {jp:"あなた", romaji:"anata", en:"you", bn:"তুমি / আপনি"}
-        ]
-};
