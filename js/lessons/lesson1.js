3b4fd8ddfa2412d967b3c3936ebf3797c8e4026a
Najmul Hasan <32976012+najmul-hasan-nirob@users.noreply.github.com>
2026-08-09 10:43:33 +0600

    Split Lesson 1 data

---
+++ js/lessons/lesson1.js
@@ -0,0 +1,8 @@
+// lesson1 data
+const lesson1 = {
+
+        vocabulary: [
+            {jp:"わたし", romaji:"watashi", en:"I", bn:"আমি"},
+            {jp:"あなた", romaji:"anata", en:"you", bn:"তুমি / আপনি"}
+        ]
+};
