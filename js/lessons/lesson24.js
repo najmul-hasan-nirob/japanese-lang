e6fe0bd17f5d816dd3010fb65c3dfb4e202841d1
Najmul Hasan <32976012+najmul-hasan-nirob@users.noreply.github.com>
2026-08-25 08:13:29 +0600

    Audit and correct Bangla meanings in Lesson 24

--- js/lessons/lesson24.js
+++ js/lessons/lesson24.js
@@ -1,117 +1,12 @@
 // Lesson 24
 const lesson24 = {
   "vocabulary": [
-    {
-      "jp": "くれます",
-      "romaji": "kuremasu",
-      "en": "give (me)",
-      "bn": "দেওয়া / আমাকে দেওয়া"
-    },
-    {
-      "jp": "なおします",
-      "romaji": "naoshimasu",
-      "en": "repair, correct",
-      "bn": "মেরামত করা"
-    },
-    {
-      "jp": "つれていきます",
-      "romaji": "tsureteikimasu",
-      "en": "take (someone)",
-      "bn": "কাউকে নিয়ে যাওয়া"
-    },
-    {
-      "jp": "つれてきます",
-      "romaji": "tsuretekimasu",
-      "en": "bring (someone)",
-      "bn": "কাউকে নিয়ে আসা"
-    },
-    {
-      "jp": "おくります[ひとを～]",
-      "romaji": "okurimasu[hitoo～]",
-      "en": "escort [someone], go with",
-      "bn": "সঙ্গ দেওয়া / সঙ্গে যাওয়া"
-    },
-    {
-      "jp": "しょうかいします",
-      "romaji": "shoukaishimasu",
-      "en": "introduce",
-      "bn": "পরিচয় করিয়ে দেওয়া"
-    },
-    {
-      "jp": "あんないします",
-      "romaji": "annaishimasu",
-      "en": "show around, show the way",
-      "bn": "ঘুরিয়ে দেখানো / পথ দেখানো"
-    },
-    {
-      "jp": "せつめいします",
-      "romaji": "setsumeishimasu",
-      "en": "explain",
-      "bn": "ব্যাখ্যা করা"
-    },
-    {
-      "jp": "おじいさん／おじいちゃん",
-      "romaji": "ojiisan／ojiichan",
-      "en": "grandfather, old man",
-      "bn": "দাদা / নানা / বৃদ্ধ ব্যক্তি"
-    },
-    {
-      "jp": "おばあさん／おばあちゃん",
-      "romaji": "obaasan／obaachan",
-      "en": "grandmother, old woman",
-      "bn": "দাদি / নানি / বৃদ্ধা"
-    },
-    {
-      "jp": "じゅんび",
-      "romaji": "junbi",
-      "en": "preparation (〜[を]します: prepare)",
-      "bn": "প্রস্তুতি"
-    },
-    {
-      "jp": "ひっこし",
-      "romaji": "hikkoshi",
-      "en": "moving out (〜[を]します: move out)",
-      "bn": "বাড়ি ছেড়ে চলে যাওয়া"
-    },
-    {
-      "jp": "[お]かし",
-      "romaji": "[o]kashi",
-      "en": "sweets, snacks",
-      "bn": "মিষ্টি / নাশতা"
-    },
-    {
-      "jp": "ホームステイ",
-      "romaji": "ho-musutei",
-      "en": "homestay",
-      "bn": "হোমস্টে / পরিবারের সঙ্গে থাকা"
-    },
-    {
-      "jp": "ぜんぶ",
-      "romaji": "zenbu",
-      "en": "all",
-      "bn": "সব / সকল"
-    },
-    {
-      "jp": "じぶんで",
-      "romaji": "jibunde",
-      "en": "by oneself",
-      "bn": "নিজে নিজে / একা"
-    }
+    {"jp":"くれます","romaji":"kuremasu","en":"give (me)","bn":"আমাকে দেওয়া / আমাকে উপহার দেওয়া"},{"jp":"なおします","romaji":"naoshimasu","en":"repair, correct","bn":"মেরামত করা / সংশোধন করা"},{"jp":"つれていきます","romaji":"tsureteikimasu","en":"take (someone)","bn":"কাউকে নিয়ে যাওয়া"},{"jp":"つれてきます","romaji":"tsuretekimasu","en":"bring (someone)","bn":"কাউকে নিয়ে আসা"},{"jp":"おくります[ひとを～]","romaji":"okurimasu[hitoo～]","en":"escort [someone], go with","bn":"[কাউকে] পৌঁছে দিতে যাওয়া / সঙ্গে করে নিয়ে যাওয়া"},{"jp":"しょうかいします","romaji":"shoukaishimasu","en":"introduce","bn":"পরিচয় করিয়ে দেওয়া"},{"jp":"あんないします","romaji":"annaishimasu","en":"show around, show the way","bn":"ঘুরিয়ে দেখানো / পথ দেখানো"},{"jp":"せつめいします","romaji":"setsumeishimasu","en":"explain","bn":"ব্যাখ্যা করা"},{"jp":"おじいさん／おじいちゃん","romaji":"ojiisan／ojiichan","en":"grandfather, old man","bn":"দাদা / নানা / বৃদ্ধ ব্যক্তি"},{"jp":"おばあさん／おばあちゃん","romaji":"obaasan／obaachan","en":"grandmother, old woman","bn":"দাদি / নানি / বৃদ্ধা"},{"jp":"じゅんび","romaji":"junbi","en":"preparation (〜[を]します: prepare)","bn":"প্রস্তুতি / প্রস্তুত করা"},{"jp":"ひっこし","romaji":"hikkoshi","en":"moving out (〜[を]します: move out)","bn":"বাড়ি বদল / এক জায়গা থেকে অন্য জায়গায় চলে যাওয়া"},{"jp":"[お]かし","romaji":"[o]kashi","en":"sweets, snacks","bn":"মিষ্টি / মিষ্টিজাতীয় খাবার / নাশতা"},{"jp":"ホームステイ","romaji":"ho-musutei","en":"homestay","bn":"হোমস্টে / স্থানীয় পরিবারের সঙ্গে থাকা"},{"jp":"ぜんぶ","romaji":"zenbu","en":"all","bn":"সব / সবকিছু"},{"jp":"じぶんで","romaji":"jibunde","en":"by oneself","bn":"নিজে / নিজের হাতে"}
   ],
   "cpart": [
-    {
-      "jp": "ほかに",
-      "romaji": "hokani",
-      "en": "besides",
-      "bn": "এছাড়াও / ছাড়াও"
-    }
+    {"jp":"ほかに","romaji":"hokani","en":"besides","bn":"এছাড়া / অন্য কিছু"}
   ],
   "country": [
-    {
-      "jp": "ははのひ",
-      "romaji": "hahanohi",
-      "en": "Mother's Day",
-      "bn": "মা / দিন"
-    }
+    {"jp":"ははのひ","romaji":"hahanohi","en":"Mother's Day","bn":"মা দিবস / Mother's Day"}
   ]
 };
