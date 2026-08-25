50025c4dccd680ee9b7e6634305669f4656060c1
Najmul Hasan <32976012+najmul-hasan-nirob@users.noreply.github.com>
2026-08-25 08:13:34 +0600

    Audit and correct Bangla meanings in Lesson 25

--- js/lessons/lesson25.js
+++ js/lessons/lesson25.js
@@ -1,111 +1,12 @@
 // Lesson 25
 const lesson25 = {
   "vocabulary": [
-    {
-      "jp": "かんがえます",
-      "romaji": "kangaemasu",
-      "en": "think, consider",
-      "bn": "ভাবা / বিবেচনা করা"
-    },
-    {
-      "jp": "つきます",
-      "romaji": "tsukimasu",
-      "en": "arrive",
-      "bn": "পৌঁছানো"
-    },
-    {
-      "jp": "とります[としを～]",
-      "romaji": "torimasu[toshio～]",
-      "en": "grow old",
-      "bn": "বয়স বাড়া / বৃদ্ধ হওয়া"
-    },
-    {
-      "jp": "たります",
-      "romaji": "tarimasu",
-      "en": "be enough, be sufficient",
-      "bn": "যথেষ্ট হওয়া"
-    },
-    {
-      "jp": "いなか",
-      "romaji": "inaka",
-      "en": "countryside, hometown",
-      "bn": "গ্রামাঞ্চল / নিজ শহর"
-    },
-    {
-      "jp": "チャンス",
-      "romaji": "chiャnsu",
-      "en": "chance",
-      "bn": "সুযোগ"
-    },
-    {
-      "jp": "おく",
-      "romaji": "oku",
-      "en": "hundred million",
-      "bn": "দশ কোটি"
-    },
-    {
-      "jp": "もし[～たら]",
-      "romaji": "moshi[～tara]",
-      "en": "if",
-      "bn": "যদি"
-    },
-    {
-      "jp": "いみ",
-      "romaji": "imi",
-      "en": "meaning",
-      "bn": "অর্থ"
-    }
+    {"jp":"かんがえます","romaji":"kangaemasu","en":"think, consider","bn":"ভাবা / বিবেচনা করা"},{"jp":"つきます","romaji":"tsukimasu","en":"arrive","bn":"পৌঁছানো / পৌঁছে যাওয়া"},{"jp":"とります[としを～]","romaji":"torimasu[toshio～]","en":"grow old","bn":"বয়স বাড়া / বয়স্ক হওয়া"},{"jp":"たります","romaji":"tarimasu","en":"be enough, be sufficient","bn":"যথেষ্ট হওয়া / পর্যাপ্ত হওয়া"},{"jp":"いなか","romaji":"inaka","en":"countryside, hometown","bn":"গ্রামাঞ্চল / নিজের গ্রামের বাড়ি বা স্বদেশের এলাকা"},{"jp":"チャンス","romaji":"chiャnsu","en":"chance","bn":"সুযোগ"},{"jp":"おく","romaji":"oku","en":"hundred million","bn":"১০ কোটি (একশ মিলিয়ন)"},{"jp":"もし[～たら]","romaji":"moshi[～tara]","en":"if","bn":"যদি / যদি ~ হয়"},{"jp":"いみ","romaji":"imi","en":"meaning","bn":"অর্থ / মানে"}
   ],
   "cpart": [
-    {
-      "jp": "もしもし",
-      "romaji": "moshimoshi",
-      "en": "hello (used on the phone)",
-      "bn": "হ্যালো (ফোনে)"
-    },
-    {
-      "jp": "てんきん",
-      "romaji": "tenkin",
-      "en": "transfer (〜します: be transferred to another office)",
-      "bn": "অফিস বদলি / স্থানান্তরিত হওয়া"
-    },
-    {
-      "jp": "こと",
-      "romaji": "koto",
-      "en": "thing, matter (〜の こと: thing about ~)",
-      "bn": "বিষয় / ব্যাপার"
-    },
-    {
-      "jp": "ひま",
-      "romaji": "hima",
-      "en": "free time",
-      "bn": "অবসর সময়"
-    },
-    {
-      "jp": "[いろいろ] おせわに なりました。",
-      "romaji": "[iroiro] osewani narimashita。",
-      "en": "Thank you for everything you've done for me.",
-      "bn": "আমার জন্য এত কিছু করার জন্য আপনাকে ধন্যবাদ।"
-    },
-    {
-      "jp": "がんばります",
-      "romaji": "ganbarimasu",
-      "en": "do one's best",
-      "bn": "সর্বোচ্চ চেষ্টা করা"
-    },
-    {
-      "jp": "どうぞ おげんきで。",
-      "romaji": "douzo ogenkide。",
-      "en": "Please take care of yourself. (said when expecting a long separation)",
-      "bn": "দীর্ঘ বিচ্ছেদের আগে নিজের যত্ন নেবেন"
-    }
+    {"jp":"もしもし","romaji":"moshimoshi","en":"hello (used on the phone)","bn":"হ্যালো (ফোনে কথা বলার সময়)"},{"jp":"てんきん","romaji":"tenkin","en":"transfer (〜します: be transferred to another office)","bn":"কর্মস্থল বদলি / অন্য অফিসে স্থানান্তর"},{"jp":"こと","romaji":"koto","en":"thing, matter (〜の こと: thing about ~)","bn":"বিষয় / ব্যাপার"},{"jp":"ひま","romaji":"hima","en":"free time","bn":"অবসর সময় / ফাঁকা সময়"},{"jp":"[いろいろ] おせわに なりました。","romaji":"[iroiro] osewani narimashita。","en":"Thank you for everything you've done for me.","bn":"আমার জন্য এত কিছু করার জন্য আপনাকে ধন্যবাদ।"},{"jp":"がんばります","romaji":"ganbarimasu","en":"do one's best","bn":"সাধ্যমতো সর্বোচ্চ চেষ্টা করা"},{"jp":"どうぞ おげんきで。","romaji":"douzo ogenkide。","en":"Please take care of yourself. (said when expecting a long separation)","bn":"নিজের যত্ন নেবেন। / ভালো থাকবেন। (দীর্ঘদিন আলাদা থাকার আগে বলা হয়)"}
   ],
   "country": [
-    {
-      "jp": "ベトナム",
-      "romaji": "betonamu",
-      "en": "Vietnam",
-      "bn": "ভিয়েতনাম"
-    }
+    {"jp":"ベトナム","romaji":"betonamu","en":"Vietnam","bn":"ভিয়েতনাম"}
   ]
 };
