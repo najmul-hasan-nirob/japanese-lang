b7e224b59b97ea8d0560759636c9c5df7a5262dc
Najmul Hasan <ss.nirob.ss@gmail.com>
2026-08-28 11:28:03 +0600

    Update kanji-manual-data.js

--- js/kanji-manual-data.js
+++ js/kanji-manual-data.js
@@ -28,25 +28,23 @@
 // furigana is optional. You can add multiple examples to one Kanji.
 
 window.kanjiManualData = [
-  // Add your Kanji entries here.
-  // Example:
-  // {
-  //   no: 1,
-  //   kanji: "山",
-  //   level: "N5",
-  //   meaning: "পাহাড়",
-  //   kun: ["やま"],
-  //   on: ["サン"],
-  //   examples: [
-  //     {
-  //       word: "富士山",
-  //       reading: "ふじさん",
-  //       romaji: "fujisan",
-  //       meaning: "ফুজি পাহাড়",
-  //       furigana: [
-  //         { kanji: "富士", reading: "ふじ" }
-  //       ]
-  //     }
-  //   ]
-  // }
+  {
+    no: 1,
+    kanji: "山",
+    level: "N5",
+    meaning: "পাহাড়",
+    kun: ["やま"],
+    on: ["サン"],
+    examples: [
+      {
+        word: "富士山",
+        reading: "ふじさん",
+        romaji: "fujisan",
+        meaning: "ফুজি পাহাড়",
+        furigana: [
+          { kanji: "富士", reading: "ふじ" }
+        ]
+      }
+    ]
+  }
 ];
