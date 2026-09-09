adbcfe703ac6e1870b0163c59b42a8180b56d8b9
Najmul Hasan <32976012+najmul-hasan-nirob@users.noreply.github.com>
2026-09-09 11:05:53 +0600

    Add first Similar Words Why vocabulary test card

---
+++ data/similar-words.js
@@ -0,0 +1,12 @@
+// Similar Words vocabulary data
+window.similarWordsData = {
+  why: [
+    {
+      jp: "なんで",
+      romaji: "nande",
+      en: "কেন",
+      bn: "কেন",
+      type: "why"
+    }
+  ]
+};
