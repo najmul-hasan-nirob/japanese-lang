d876c3fd65dc6278928da6e6b401a706501f6909
Najmul Hasan <32976012+najmul-hasan-nirob@users.noreply.github.com>
2026-09-10 18:56:57 +0600

    Update Important Rules

--- js/important-rules.js
+++ js/important-rules.js
@@ -1,6 +1,6 @@
 // Japanese Lang — Important Rules data
 const importantRules = [
-    { kind: "Test frontt", rule: "Test backk" }
+    { kind: "が এর ব্যবহার", rule: "৩টি verb (わかります、あります、います) ও ৪টি なadjective (すき、きらい、じょうず、へた) সহ মোট ৭টির এর আগে が পারটিকেল বসবেই।" }
 ];
 window.importantRules = importantRules;
 
